3ec227b199984009c6d8a7df7f0fee1fffd47874
bampro <bampro@hotmail.com>
2016-02-09 19:08:17 +0700

    delete comment

--- api/connect.php
+++ api/connect.php
@@ -22,7 +22,4 @@ function nDB () {
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $dbh;
-    //ทดสอบ
-    //ทดสอบ ทดสอบ
-    //test test
 }
